bff9113d2e5ea8a184545e07a8c65bfe70b1d700
cengizhan94 <sonsair11@gmail.com>
2023-07-16 15:58:58 +0300

    updating models

--- polls/models.py
+++ polls/models.py
@@ -11,6 +11,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -19,5 +22,4 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
     
-    def was_published(self):
-        return self.pub_date >= timezone.now() - datetime.timedata(days=1)
+    
